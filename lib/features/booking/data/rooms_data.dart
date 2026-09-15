21676962bf3a3b95445181056b40f3798e910e78
muneerahamed.dev@gmail.com <muneerahamed.dev@gmail.com>
2026-09-15 21:45:07 +0530

    Added room data and model

---
+++ lib/features/booking/data/rooms_data.dart
@@ -0,0 +1,9 @@
+import 'models/room.dart';
+
+const sampleRooms = <Room>[
+  Room(id: 'R101', name: 'Deluxe Room', pricePerNight: 3500, maxGuests: 2),
+  Room(id: 'R102', name: 'Deluxe Room', pricePerNight: 3500, maxGuests: 2),
+  Room(id: 'R201', name: 'Executive Suite', pricePerNight: 5800, maxGuests: 3),
+  Room(id: 'R202', name: 'Executive Suite', pricePerNight: 5800, maxGuests: 3),
+  Room(id: 'R301', name: 'Family Room', pricePerNight: 4200, maxGuests: 4),
+];
